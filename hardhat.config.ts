c783ba150d0baa9bb13a71f0b70ccc64a00eb6cd
Emmett Childs <emmettchilds@thegobospot.com>
2022-03-27 06:34:35 +0800

    added typescript dependencies to project

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: "0.8.9",
+};
+
+/*
+npm i -D typescript typechain ts-node @typechain/ethers-v5 @typechain/hardhat @types/ch
+ai @types/node
+*/
